3088eed00b6eba3606a609f921a8eb225d43e1e8
andrew <andre@DESKTOP-MLF7TAD>
2020-10-21 23:39:17 +1100

    Fixed release build Removed unneeded files from source control

--- ScummVmBrowser/ScummVMBrowser/Scripts/ScummVMBrowser/Scripts/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const ReactDOM = require("react-dom");
-const Router_1 = require("./components/Router");
-ReactDOM.render(React.createElement("div", null,
-    React.createElement(Router_1.MainRouter, null)), document.getElementById("app"));
-//# sourceMappingURL=index.js.map
