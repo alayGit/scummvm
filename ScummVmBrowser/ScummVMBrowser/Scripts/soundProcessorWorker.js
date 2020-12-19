57f07097c9d7cd03ba5d7f7bf808be22dc7210ad
andrew <andre@DESKTOP-MLF7TAD>
2020-12-19 15:15:31 +1100

    Beginnings of AAC

--- ScummVmBrowser/ScummVMBrowser/Scripts/soundProcessorWorker.js
+++ ScummVmBrowser/ScummVMBrowser/Scripts/soundProcessorWorker.js
@@ -2,6 +2,8 @@
 self.onmessage = function (e) {
 	importScripts("/scripts/pako_inflate.min.js");
 	importScripts("/scripts/yEncoding.js");
+	importScripts("/scripts/aurora/aurora.js");
+	importScripts("/scripts/aurora/decoders/aac.js");
 	var deencoded = DecodeYEncode(e.data);
 	var compressedUInt8 = Uint8Array.from(deencoded);
 
