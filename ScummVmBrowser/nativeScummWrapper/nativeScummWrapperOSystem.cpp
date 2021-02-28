4a4a84b4a4a695f7d911d60f8930baf2d31da602
andrew <andre@DESKTOP-MLF7TAD>
2021-02-28 18:29:31 +1100

    Missed this fix

--- ScummVmBrowser/nativeScummWrapper/nativeScummWrapperOSystem.cpp
+++ ScummVmBrowser/nativeScummWrapper/nativeScummWrapperOSystem.cpp
@@ -5,7 +5,7 @@
 #include "nativeScummWrapperOSystem.h"
 
 NativeScummWrapper::NativeScummWrapperOSystem::NativeScummWrapperOSystem(SoundManagement::SoundOptions soundOptions, f_SendScreenBuffers sendScreenBuffers, f_PollEvent queueEvent, f_SaveFileData saveData, SoundManagement::f_PlaySound playSound) : ModularBackend() {
-	_mixerImpl = nullptr;	
+	_mixerImpl = nullptr;
 	_soundOptions = soundOptions;
 	_fsFactory = new WindowsFilesystemFactory();
 	_cliGraphicsManager = new NativeScummWrapperGraphics(sendScreenBuffers);
@@ -20,9 +20,6 @@ NativeScummWrapper::NativeScummWrapperOSystem::NativeScummWrapperOSystem(SoundMa
 	_soundProcessor = new SoundManagement::SoundProcessor();
 }
 
-
-
-
 NativeScummWrapper::NativeScummWrapperOSystem::~NativeScummWrapperOSystem() {
 	_soundThreadManager->StopSound(true);
 	delete _soundThreadManager;
@@ -31,12 +28,12 @@ NativeScummWrapper::NativeScummWrapperOSystem::~NativeScummWrapperOSystem() {
 }
 
 void NativeScummWrapper::NativeScummWrapperOSystem::initBackend() {
-	Audio::MixerImpl* mixer = new Audio::MixerImpl(_soundOptions.sampleRate);
+	Audio::MixerImpl *mixer = new Audio::MixerImpl(_soundOptions.sampleRate);
 	mixer->setReady(true);
 	_mixer = mixer;     //Super class type variable required by ScummVM
 	_mixerImpl = mixer; //The subclass variable for our convenience
 
-	std::function<byte* (byte*, int)> mixCallBack = [&](byte* x, int y) {
+	std::function<byte *(byte *, int)> mixCallBack = [&](byte *x, int y) {
 		return this->mixCallback(x, y);
 	};
 
@@ -56,20 +53,29 @@ void NativeScummWrapper::NativeScummWrapperOSystem::delayMillis(uint msecs) {
 	std::this_thread::sleep_for(std::chrono::milliseconds(msecs));
 }
 
-void NativeScummWrapper::NativeScummWrapperOSystem::getTimeAndDate(TimeDate& t) const {
-	//TODO Implement
+void NativeScummWrapper::NativeScummWrapperOSystem::getTimeAndDate(TimeDate &t) const {
+	time_t now = time(0);
+	tm *gmtm = gmtime(&now);
+
+	t.tm_hour = gmtm->tm_hour;
+	t.tm_mday = gmtm->tm_mday;
+	t.tm_min = gmtm->tm_min;
+	t.tm_mon = gmtm->tm_mon;
+	t.tm_sec = gmtm->tm_sec;
+	t.tm_wday = gmtm->tm_wday;
+	t.tm_year = gmtm->tm_year;
 }
 
-void NativeScummWrapper::NativeScummWrapperOSystem::logMessage(LogMessageType::Type type, const char* message) {
+void NativeScummWrapper::NativeScummWrapperOSystem::logMessage(LogMessageType::Type type, const char *message) {
 }
 
-void NativeScummWrapper::NativeScummWrapperOSystem::setGameSaveCache(SaveFileCache* cache) {
-	((NativeScummVmSaveManager*)_savefileManager)->setGameSaveCache(cache);
+void NativeScummWrapper::NativeScummWrapperOSystem::setGameSaveCache(SaveFileCache *cache) {
+	((NativeScummVmSaveManager *)_savefileManager)->setGameSaveCache(cache);
 }
 
 const int NO_CHANNELS = 2; //TODO: Read this value from Scumm VM don't assume 2
-byte* NativeScummWrapper::NativeScummWrapperOSystem::mixCallback(byte* samples, int sampleSize) {
-	_mixerImpl->mixCallback((uint8_t*)samples, sampleSize);
+byte *NativeScummWrapper::NativeScummWrapperOSystem::mixCallback(byte *samples, int sampleSize) {
+	_mixerImpl->mixCallback((uint8_t *)samples, sampleSize);
 
 	return samples;
 }
@@ -84,17 +90,16 @@ void NativeScummWrapper::NativeScummWrapperOSystem::StartSound() {
 }
 
 void NativeScummWrapper::NativeScummWrapperOSystem::quit() {
-
 }
 
 void NativeScummWrapper::NativeScummWrapperOSystem::StopSound() {
 	_soundThreadManager->StopSound(false);
 }
 
-NativeScummWrapper::NativeScummWrapperGraphics* NativeScummWrapper::NativeScummWrapperOSystem::getGraphicsManager() {
+NativeScummWrapper::NativeScummWrapperGraphics *NativeScummWrapper::NativeScummWrapperOSystem::getGraphicsManager() {
 	return _cliGraphicsManager;
 }
 
-Common::EventSource* NativeScummWrapper::NativeScummWrapperOSystem::getDefaultEventSource() {
+Common::EventSource *NativeScummWrapper::NativeScummWrapperOSystem::getDefaultEventSource() {
 	return _eventSource;
 }
