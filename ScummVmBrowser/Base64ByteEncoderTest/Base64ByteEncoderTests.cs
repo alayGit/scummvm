14d25cf9062e7e6b6611b757909031323ca9650e
andrew <andre@DESKTOP-MLF7TAD>
2021-04-26 18:43:33 +1000

    Base64 Byte Encoder Tests

---
+++ ScummVmBrowser/Base64ByteEncoderTest/Base64ByteEncoderTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Base64ByteEncoderTest
+{
+    [TestClass]
+    public class Base64ByteEncoderTests
+    {
+        [TestMethod]
+        public void CanDecodeAndEncode()
+        {
+			byte[] TestData = new byte[] { 12, 33, 44 };
+
+			Base64ByteEncoder.Base64ByteEncoder e = new Base64ByteEncoder.Base64ByteEncoder();
+
+			Assert.IsTrue(TestData.SequenceEqual(e.ByteDecode(e.ByteEncode(TestData))));
+        }
+    }
+}
---
+++ ScummVmBrowser/Base64ByteEncoderTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Base64ByteEncoderTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Base64ByteEncoderTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e06081e2-02ef-4da2-9138-f5a0b19922d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
