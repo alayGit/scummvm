5f110c9a6bb1b0aa83892888766eaf06cb894815
andrew <andre@DESKTOP-MLF7TAD>
2021-04-27 20:57:06 +1000

    Tidy

--- ScummVmBrowser/nativeScummWrapper/mouseState.h
+++ ScummVmBrowser/nativeScummWrapper/mouseState.h
@@ -20,7 +20,6 @@ namespace NativeScummWrapper {
 	    int prevHotX;
 	    int prevHotY;
 		const void *buffer;
-		//PalletteColor *cursorPallette;
 		byte keyColor;
 
 		int adjustedX()
@@ -56,7 +55,6 @@ namespace NativeScummWrapper {
 			width = -1;
 			height = -1;
 			buffer = nullptr;
-			///cursorPallette = nullptr;
 			prevX = -1;
 			prevY = -1;
 			prevW = -1;
